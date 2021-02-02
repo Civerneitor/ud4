ab3e9f074006f693d508c27926b0e3dfaed129cc
Sergio <sergio_castilla_13@hotmail.com>
2021-02-02 18:02:23 +0100

    ejercicio3

---
+++ ud4-3/src/ejercicio3.java
@@ -0,0 +1,31 @@
+
+public class ejercicio3 {
+	public static void main(String[] args) {
+		int X = 1;
+		int Y = 2;
+		double N = 4;
+		double M = 2.3;
+		
+		System.out.println("Variable X="+X);
+		System.out.println("Variable Y="+Y);
+		System.out.println("Variable N="+N);
+		System.out.println("Variable M="+M);
+		System.out.println(X+" + "+Y+" = " +(X+Y));
+		System.out.println(X+" - "+Y+" = " +(X-Y));
+		System.out.println(X+" * "+Y+" = " +(X*Y));
+		System.out.println(X+" / "+Y+" = " +(X/Y));
+		System.out.println(X+" % "+Y+" = " +(X%Y));
+		System.out.println(N+" + "+M+" = " +(N+M));
+		System.out.println(N+" - "+M+" = " +(N-M));
+		System.out.println(N+" * "+M+" = " +(N*M));
+		System.out.println(N+" / "+M+" = " +(N/M));
+		System.out.println(N+" % "+M+" = " +(N%M));
+		System.out.println("El doble de X = "+(X*2));
+		System.out.println("El doble de Y = "+(Y*2));
+		System.out.println("El doble de N = "+(N*2));
+		System.out.println("El doble de M = "+(M*2));
+		System.out.println("La suma de todas las variables = "+(X+Y+N+M));
+		System.out.println("El producto de todas las variables = "+(X*Y*N*M));
+
+	}
+}
