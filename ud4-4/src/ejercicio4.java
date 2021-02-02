515d618b05c4b19b9e7a7e4c76f9c4655bea6028
Sergio <sergio_castilla_13@hotmail.com>
2021-02-02 18:04:32 +0100

    ejercicio4

---
+++ ud4-4/src/ejercicio4.java
@@ -0,0 +1,16 @@
+
+public class ejercicio4 {
+	public static void main(String[] args) {
+
+		int N = 4;
+		System.out.println("Variable N="+N);
+		N+=77;
+		System.out.println("Variable N="+N);
+		N-=3;
+		System.out.println("Variable N="+N);
+		N*=2;
+		System.out.println("Variable N="+N);
+		
+		
+	}
+}
