3bf294e3ff35efeefa224e22955efb645e46d552
Sergio <sergio_castilla_13@hotmail.com>
2021-02-02 18:05:27 +0100

    ejercicio5

---
+++ ud4-5/src/ejercicio5.java
@@ -0,0 +1,21 @@
+
+public class ejercicio5 {
+	public static void main(String[] args) {
+
+		int A = 4;
+		int B = 5;
+		int C = 6;
+		int D = 7;
+		B=C;
+		C=A;
+		A=D;
+		D=B;
+
+		System.out.println("Variable A="+A);
+		System.out.println("Variable B="+B);
+		System.out.println("Variable C="+C);
+		System.out.println("Variable D="+D);
+		
+		
+	}
+}
